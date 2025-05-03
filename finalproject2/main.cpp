0bd55ee56b19be313b671f6c1c02eb6cfb8cca3f
mariyam <mariyam1908@outlook.com>
2025-05-04 00:45:08 +0500

    green circle

--- finalproject2/main.cpp
+++ finalproject2/main.cpp
@@ -1,7 +1,27 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "Pushing changes to GitHub" << endl;
-	return 0;
+int main(){
+    //create window
+    //DO NOT MAKE STYLE FULLSCREEN I LEARNT THE HARD WAY
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "tetris", sf::Style::Default);
+
+    //create shape
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    //game loop
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
